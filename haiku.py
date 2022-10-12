5567ad1a720131a9a7dfac53327bde828cf9e296
Luc <34852@ma-web.nl>
2022-10-12 19:35:55 +0200

    my haiku's

--- haiku.py
+++ haiku.py
@@ -1,6 +1,26 @@
 
 
 #plak hier de haiku functions onder elkaar
+## Haiku function template
+
+def Haiku1():
+    print("haiku by:"," Kobayahsi Issa")
+    print("the wren")
+    print("earn his living")
+    print("noiseslessly")
+
+
+def Haiku2():
+    print("haiku by:","Masaoka Shiki")
+    print("my life")
+    print("how much more of it remains")
+    print("the night is brief")
+
+def Haiku3():
+    print("haiku by:","Natsume Soseki")
+    print("The lamp once out")
+    print("cool starts enter")
+    print("the window frame")
 
 
 
